7d51f10b012a24a133e1a90d38dd21ca7ed83f25
Haoyan Li <angelgamer@gmail.com>
2015-09-06 23:39:28 +0800

    Add some code about ORM.

--- src/sqlbase.py
+++ src/sqlbase.py
@@ -4,18 +4,20 @@
 
 import sqlite3
 
+
+#TODO: Add more class method for Database Class.
 class Database():
     def __init__(self, db_name):
         self.db = sqlite3.connect(db_name.decode('utf-8'))
         # self.db = sqlite3.connect(':memory:')
         self.cr = self.db.cursor()
 
-    def create_table(self, table_name, columns):
+    def create_table(self, table_name, columns_detail):
         command = [
             ('', False),
             'CREATE TABLE',
             table_name,
-            columns
+            columns_detail
         ]
         # print create_str(command)
 
@@ -36,50 +38,73 @@ class Database():
         except sqlite3.OperationalError, e:
             print 'Exception: ' + e.message
 
-    def query(self, query_name, query_column):
+    def query(self, table_name, columns_name, conditions):
         command = [
             ('', False),
             'SELECT',
-            query_name,
+            columns_name,
             'FROM',
-            query_column
+            table_name,
+            'WHERE',
+            conditions
+        ]
+        print create_str(command)
+        self.cr.execute(create_str(command))
+        return self.cr.fetchall()
+
+    def update(self, table_name, values, conditions):
+        command = [
+            ('', False),
+            'UPDATE',
+            table_name,
+            'SET',
+            values,
+            'WHERE',
+            conditions
         ]
 
+        print create_str(command)
         self.cr.execute(create_str(command))
-
-    def update(self, table_name, value, column_name):
-        pass
-        # self.cr.execute('UPDATE ')
+        self.db.commit()
 
     def insert(self, table_name, values):
+        query_qmark = ['?' for dummy_idx in range(len(values))]
+        query_qmark.insert(0, (',', True))
+
         command = [
             ('', False),
             'INSERT INTO',
             table_name,
             'VALUES',
-            values
+            query_qmark
         ]
-
-        self.cr.execute(create_str(command))
+        print create_str(command)
+        self.cr.execute(create_str(command), values)
         self.db.commit()
 
+    def disconnect(self):
+        self.db.close()
 
-# def create_tuple_str(target_tuple, sep=''):
-#     tuple_str = u''
-#     for element in target_tuple:
-#         tuple_str += element.decode('utf-8') + sep.decode('utf-8') + u' '
-#     return u'(' + tuple_str[:-len(sep.decode('utf-8') + u' ')] + u')'
 
-def create_str(target_tuple):
+
+def create_str(target):
     return_str = u''
-    sep = target_tuple[0][0]
-    hasBracket = target_tuple[0][1]
 
-    for element in target_tuple[1:]:
-        if isinstance(element, tuple) or isinstance(element, list):
-            return_str += create_str(element) + sep.decode('utf-8') + u' '
-        else:
-            return_str += element.decode('utf-8') + sep.decode('utf-8') + u' '
+    if isinstance(target, dict):
+        iter_target = target.copy()
+        sep = iter_target.pop('sep')
+        hasBracket = iter_target.pop('hasBracket')
+        for element_key, element_value in iter_target.iteritems():
+            return_str += (element_key.decode('utf-8') + u'=' + element_value.decode('utf-8') +
+                           sep.decode('utf-8') + u' ')
+    else:
+        sep = target[0][0]
+        hasBracket = target[0][1]
+        for element in target[1:]:
+            if isinstance(element, tuple) or isinstance(element, list) or isinstance(element, dict):
+                return_str += create_str(element) + sep.decode('utf-8') + u' '
+            else:
+                return_str += element.decode('utf-8') + sep.decode('utf-8') + u' '
 
     if hasBracket:
         return u'(' + return_str[:-len(sep.decode('utf-8') + u' ')] + u')'
@@ -87,6 +112,7 @@ def create_str(target_tuple):
         return return_str[:-len(sep.decode('utf-8') + u' ')]
 
 # columns = [
+
 #     (',', True),
 #     (('', False), 'Word', 'TEXT'),
 #     (('', False), 'Pronunciation', 'TEXT'),
@@ -121,6 +147,9 @@ if __name__ == '__main__':
     ]
     # print create_tuple_str(columns[1], ',')
     # db.delete_table('Vocabulary')
-    db.create_table('Vocabulary', columns)
-    # db.insert('Vocabulary', (u'A', u'A', u'A', u'A', u'A'))
+    # db.create_table('Vocabulary', columns)
+    # db.insert('Vocabulary', ('A', 'A', 'A', 'A', 'A'))
+    # db.update('Vocabulary', {'sep': ',', 'hasBracket': False, 'Word': '\'abandon\'', 'Pronunciation': '\'aa\''},
+    #           {'sep': '', 'hasBracket': False, 'Word': '\'A\''})
+    print db.query('Vocabulary', ((',', False), 'Word'), {'sep': '', 'hasBracket': False, 'Word': '\'abandon\''})
     pass
